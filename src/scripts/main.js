853baebdb41b923fd5cedcb3a7f7f0613275b322
Daria <darialys2510@gmail.com>
2025-06-12 13:59:00 +0200

    add fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,20 @@ const tree = document.querySelector('.tree');
 
 for (const li of document.querySelectorAll('li')) {
   const span = document.createElement('span');
+  const textNode = li.firstChild;
 
-  span.append(...li.childNodes);
-
-  li.append(span);
+  if (textNode && textNode.nodeType === Node.TEXT_NODE) {
+    li.insertBefore(span, textNode);
+    span.appendChild(textNode);
+  }
 }
 
-tree.onclick = function (e) {
+tree.addEventListener('click', (e) => {
   if (e.target.tagName === 'SPAN') {
     const childrenContainer = e.target.parentNode.querySelector('ul');
 
     if (childrenContainer) {
       childrenContainer.hidden = !childrenContainer.hidden;
-    } 
+    }
   }
-};
+});
